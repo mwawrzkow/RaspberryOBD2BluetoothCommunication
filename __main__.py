c07a3f77c3054fb5b10a3d235c9bffb19a833cdc
Marcin Wawrzków <marcin.wawrzkow@outlook.com>
2020-10-06 16:42:13 +0200

    test

--- __main__.py
+++ __main__.py
@@ -1,9 +1,16 @@
-import bluetootha as bluetooth
+import bluetooth
 
+server_sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
 
+port = 1
+server_sock.bind(("",port))
+server_sock.listen(1)
 
-if __name__ == "__main__":
-    a = bluetooth.Server()
-    a.init() 
-    a.checkData()
-    pass
+client_sock,address = server_sock.accept()
+print "Accepted connection from ",address
+
+data = client_sock.recv(1024)
+print "received [%s]" % data
+
+client_sock.close()
+server_sock.close()
