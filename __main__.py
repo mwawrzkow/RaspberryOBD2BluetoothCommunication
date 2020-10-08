ea3239b648d0ac3de1b102f73bc8d89853a0f2c1
Marcin Wawrzków <marcin.wawrzkow@outlook.com>
2020-10-08 10:42:38 +0200

    abc

--- __main__.py
+++ __main__.py
@@ -6,7 +6,7 @@ server_sock.bind(("",0))
 server_sock.listen(1)
 print ("listening on port: 0")
 
-uuid = "1e0ca4ea-299d-4335-93eb-27fcfe7fa848"
+uuid = "5dc85d5b-4bdd-40ca-ab9c-c700c088c6c4"
 bluetooth.advertise_service( server_sock, "FooBar Service", uuid )
 
 client_sock,address = server_sock.accept()
