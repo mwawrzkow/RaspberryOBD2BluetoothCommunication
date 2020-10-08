e67cbb99e6016b9ded715ed0d592bec5e8ba25db
Marcin Wawrzków <marcin.wawrzkow@outlook.com>
2020-10-08 14:47:44 +0100

    WORKING

--- __main__.py
+++ __main__.py
@@ -10,10 +10,13 @@ uuid = "5dc85d5b-4bdd-40ca-ab9c-c700c088c6c4"
 bluetooth.advertise_service( server_sock, "FooBar Service", uuid )
 
 client_sock,address = server_sock.accept()
-print ("Accepted connection from :" + address)
+print ("Accepted connection from :")
+print(address)
 
 data = client_sock.recv(1024)
-print ("received [ "+data + "]")
+print ("received [ ")
+print(data)
+print("]")
 
 client_sock.close()
 server_sock.close()
