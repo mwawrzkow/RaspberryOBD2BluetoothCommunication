64fe964ef9789d0b47c6e90f7e92bfef8381c75a
Marcin Wawrzków <marcin.wawrzkow@outlook.com>
2020-10-23 11:53:56 +0100

    .

--- __main__.py
+++ __main__.py
@@ -1,4 +1,9 @@
 import bluetooth
+import os
+
+if os.system("hciconfig hci0 piscan") == -1:
+    print("Run it as root!")
+    exit
 
 server_sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
 # port = bluetooth._get_available_ports( bluetooth.RFCOMM )
@@ -14,9 +19,16 @@ print ("Accepted connection from :")
 print(address)
 
 data = client_sock.recv(1024)
-print ("received [ ")
-print(data)
-print("]")
+while data != "exit":
+    print ("received [ ")
+    print(data)
+    print("]")
+    if data == b"EnX88hsumpCFAqBxr#ckpL!7X7G+eDCEyLGq!Bc?X-^s5$BJm*PGfD!tnBtj7f8B@5!XL=Bu#?8p$sAeWUMK=2+5HAXBe9=VhTwE":
+        print("Connection accepted")
+        client_sock.send(b"Accept")
+
+    #client_sock.send(data)
+    data = client_sock.recv(1024)
 
 client_sock.close()
 server_sock.close()
