50e193769a1f2307b64acf69407cb20d5f1868b7
Marcin Wawrzków <marcin.wawrzkow@outlook.com>
2020-10-23 13:34:21 +0200

    bluetooth socket updated

--- bluetoothWatchDog.py
+++ bluetoothWatchDog.py
@@ -1,7 +1,10 @@
 class Watchdog:
     def waitForData(self):
-        return self.Socket.recv(1024)
+        data = self.Socket.recv(1024)
+        print(data)
+        return data
     def sendData(self,data):
+        print(data)
         return self.Socket.send(data)
     def __init__(self, clientSocket):
         self.Socket = clientSocket
